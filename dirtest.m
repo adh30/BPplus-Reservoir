8c3358e879445dd8e6e7130b7cf287f7f6ebf957
Alun <alun.hughes@ucl.ac.uk>
2024-04-19 17:49:21 +0100

    update

---
+++ dirtest.m
@@ -0,0 +1,20 @@
+% testing json etc
+clear
+% get user directory
+if ispc; userdir=getenv('USERPROFILE');
+else; userdir= getenv('HOME');
+end
+% read json
+S=readstruct("config.json", FileType="json");
+A=convertStringsToChars(S.folder_name);
+datadir=[userdir A];
+% datadir=[userdir '\BPPdata'];
+if not(isfolder(datadir))
+   % set data folder
+   dirnew = uigetdir(userdir);
+   S.folder_name=dirnew;
+   writestruct(S,"config.json",PreserveInfAndNaN=false);
+   % type config.json
+else
+    disp("Data folder exists")
+end
