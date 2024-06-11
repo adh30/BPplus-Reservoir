1a9ac7b7c9e92ced0209678bd55a279c8d4c8ece
Alun <alun.hughes@ucl.ac.uk>
2024-06-11 12:33:45 +0100

    delete backups

--- temporary backups/dir1.m
+++
@@ -1,8 +0,0 @@
-% temporary - used to check code to read the default directory from the
-% config.json file - once it works I'll incorporate it into bpp_Res2.m
-clear
-jtext = fileread('config.json');
-jdata = jsondecode(jtext);
-%file_lists=dir(fullfile(string(data.folder_name), '*.*'));
-folder_name = jdata.folder_name;
-disp(folder_name)
